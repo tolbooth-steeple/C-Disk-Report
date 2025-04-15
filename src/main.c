449a2d51a17baf61e768a17d563b928dab85d5b9
a <207623952+tolbooth-steeple@users.noreply.github.com>
2025-04-15 01:53:18 +0100

    added get file size function that uses linux OS API

--- src/main.c
+++ src/main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+#include <sys/stat.h>
 
 // declare functions here
 long get_file_size(char *);
 
 // start of program
 int main() {
-  char *filename = "example.txt"
-  long filesize = get_file_size(filename)
-  printf("Size of '%s' is %ld", filename, filesize)
+  char *filename = "example.txt";
+  long filesize = get_file_size(filename);
+  printf("Size of '%s' is %ld", filename, filesize);
 }
 
 // naive solution to get file size
@@ -22,4 +23,12 @@ long get_file_size(char *filename) {
   // close the file before returning
   fclose(fileptr);
   return size;
+}
+
+// linux solution: use os API for file status
+long get_file_size_linux(char *filename) {
+  // get file information with stat structure
+  struct stat file_info;
+
+  return file_info.st_size;
 }
